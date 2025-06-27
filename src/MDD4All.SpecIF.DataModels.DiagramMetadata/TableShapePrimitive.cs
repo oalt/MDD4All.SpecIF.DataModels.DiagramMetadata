b762ce6f10878befd0cae0e76f904e4fe3eda39c
oalt <oa@lacheweg21.de>
2025-06-27 08:45:12 +0200

    Initial commit of TableShapePrimitive

---
+++ src/MDD4All.SpecIF.DataModels.DiagramMetadata/TableShapePrimitive.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.SpecIF.DataModels.DiagramMetadata
+{
+    public class TableShapePrimitive : ShapePrimitive
+    {
+    }
+}
